f10347479575445ab38a93ecf28fbe16f0f0d9b9
Sansossio <juliosansossio@gmail.com>
2020-05-11 12:28:09 +0200

    Add examples

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,3 @@
-import { Injectable } from './decorators/injectable/injectable.decorator'
-import { InjectorApp } from './app/injector-app'
-import { Property } from './decorators/property/property.decorator'
-
-class A {
-  value = 1
-}
-
-@Injectable()
-class B {
-  @Property()
-  public a: A
-}
-
-@Injectable()
-class C {
-  constructor (
-    readonly b: B
-  ) {}
-}
-
-const app = InjectorApp.create([C, A, B])
-const c = app.get(C)
-console.log(c.b.a.value)
+export { Injectable } from './decorators/injectable/injectable.decorator'
+export { InjectorApp } from './app/injector-app'
+export { Property } from './decorators/property/property.decorator'
