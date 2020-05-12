ee004475701cf1c97031f62aba524cdf4237abe0
Sansossio <juliosansossio@gmail.com>
2020-05-12 09:34:00 +0200

    Update function def

--- src/decorators/property/property.decorator.ts
+++ src/decorators/property/property.decorator.ts
@@ -3,7 +3,7 @@ import { Property } from './property.type'
 import { InjectableEnum } from '../../enum/injectable.enum'
 
 export function Property () {
-  return (target: any, key: string) => {
+  return function (target: any, key: string) {
     const fields: Property[] = Reflect.getMetadata(InjectableEnum.PROPERTIES, target) || []
     const value = Reflect.getMetadata('design:type', target, key)
     const property: Property = {
